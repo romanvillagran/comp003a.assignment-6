5a018d83316821befa8cd50054a3e55876c33eb4
Your Name <you@example.com>
2025-02-21 20:51:58 -0800

    added welcoming and other stuff. gonna start with the switch now also added case 1

--- comp003a.assignment 6/Program.cs
+++ comp003a.assignment 6/Program.cs
@@ -8,7 +8,75 @@ namespace comp003a.assignment_6
     {
         static void Main(string[] args)
         {
+            List<Animal> animal = new List<Animal>();
+            bool running = true;
 
+            while (running)
+            {
+                Console.WriteLine("Welcome to the zoo Management system");
+
+                Console.WriteLine("Please choose and option: ");
+                Console.WriteLine("1. add a Lion");
+                Console.WriteLine("2. add a Parrot");
+                Console.WriteLine("3. display all animals");
+                Console.WriteLine("4. describe an animal");
+                Console.WriteLine("5. exit");
+
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        try
+                        {
+                            Console.WriteLine("Enter the name of the lion");
+                            string lionName = Console.ReadLine();
+                            Console.WriteLine("Enter the species of the lion");
+
+                            string lionSpecies = Console.ReadLine();
+
+                            Lion lion = new Lion { Name = lionName, Species = lionSpecies};
+
+                            animal.Add(lion);
+                            Console.WriteLine("Lion has been added"); 
+                        }
+                        catch (ArgumentException ex) // Handle errors
+                        {
+                            Console.WriteLine($"Error: {ex.Message}"); // Display error message
+                        }
+                        break;
+
+
+
+                    case "2":
+                        try
+                        {
+
+                        }
+
+
+                    case "3":
+                        try
+                        {
+
+                        }
+
+
+                    case "4":
+                        try
+                        {
+
+                        }
+
+
+                    case "5":
+                        try
+                        {
+
+                        }
+
+                }
+            }
         }
     }
 }
